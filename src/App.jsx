2d79f9033360b543d56692b71bcf588677a5de43
Archangeltv <archangeltv24@gmail.com>
2023-05-30 00:52:14 +0100

    Hash

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext";
 import Home from "./pages/home/home";
 import SignUp from "./pages/signup/signup";
