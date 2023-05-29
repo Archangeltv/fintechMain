05277f87037a69113f0a4ead28fcd94cd0fb4df8
Archangeltv <archangeltv24@gmail.com>
2023-05-30 00:54:46 +0100

    Confirming

--- src/pages/account/components/BalanceCard.jsx
+++ src/pages/account/components/BalanceCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import svg1 from "../../../assets/Ellipse 184.svg";
 import svg2 from "../../../assets/Rectangle 4055.svg";
 import svg3 from "../../../assets/Rectangle 4056.svg";
-import { SiAddthis } from "react-icons/si";
+import { BsFillArrowRightCircleFill } from "react-icons/bs";
 
 const BalanceCard = ({ amount }) => {
   return (
@@ -13,10 +13,10 @@ const BalanceCard = ({ amount }) => {
         <img src={svg3} alt="" className="top-0 absolute right-0 z-0" />
         <div className="p-5 py-10 h-full flex flex-col">
           <p className="text-text text-lg font-medium">Total Balance:</p>
-          <p className="text-3xl mt-2 text-text font-medium">${amount}</p>
-          <p className="mt-2 text-sm text-text">(N750/$)</p>
+          <p className="text-3xl mt-5 text-text font-medium">${amount}</p>
+          <p className="mt-5 text-sm text-text">(N750/$)</p>
           <div className=" flex items-center gap-3 self-end justify-self-end mt-10 text-text text-lg font-medium">
-            Deposit <SiAddthis />
+            Deposit <BsFillArrowRightCircleFill />
           </div>
         </div>
       </div>
