be84c24df9d7b445fd1cd6385e373a2c25624f11
Archangeltv <archangeltv24@gmail.com>
2023-05-30 01:30:29 +0100

    Dev

--- src/pages/account/components/Bottom.jsx
+++ src/pages/account/components/Bottom.jsx
@@ -10,25 +10,25 @@ const Bottom = () => {
   return (
     <div>
       <div className="flex items-center justify-center gap-5 mt-3 flex-wrap">
-        <div className=" w-24 text-center p-3  flex flex-col items-center justify-center shadow ">
+        <div className=" w-24 text-center p-3 text-sm  flex flex-col items-center justify-center shadow ">
           <div className="text-3xl  mb-2">
             <BsSendFill />
           </div>
           Transfer
         </div>
-        <div className="w-24 text-center  p-3 flex flex-col items-center justify-center shadow">
+        <div className="w-24 text-center  p-3 text-sm flex flex-col items-center justify-center shadow">
           <div className="text-3xl  mb-2">
             <BsFillArrowRightCircleFill />
           </div>
           Deposit
         </div>
-        <div className="w-24 text-center  p-3 flex flex-col items-center justify-center shadow">
+        <div className="w-24 text-center  p-3 text-sm flex flex-col items-center justify-center shadow">
           <div className="text-3xl  mb-2">
             <MdOutlinePayments />
           </div>
           Pay Bills
         </div>
-        <div className="w-24 text-center  p-3 flex flex-col items-center justify-center shadow">
+        <div className="w-24 text-center  p-3 text-sm flex flex-col items-center justify-center shadow">
           <div className="text-3xl  mb-2">
             <BsFillSuitHeartFill />
           </div>
